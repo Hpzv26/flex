55248e66dd7fcb639a2bd2bafd06e1951f0ff869
Hpzv26 <159396342+Hpzv26@users.noreply.github.com>
2024-02-28 20:27:23 -0600

    Update global.js

--- global.js
+++ global.js
@@ -1,9 +1,9 @@
-document.querySelector("close")addEventListener("click",function()
+document.querySelector(".close").addEventListener("click",function()
 {
-console.log("click")
-document.querySelector("ventana-open").classList.remove("open")
-//document.queryselector(".ventana-open").classList.add("open")
+console.log("click");
+document.querySelector(".window").classList.remove("open");
+//document.queryselector(".ventana-open").classList.add("open");
 
 
 
-})
+});
