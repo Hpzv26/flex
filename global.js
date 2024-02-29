eb44f3caa4402deb80c9999e6a33514964142562
Hpzv26 <159396342+Hpzv26@users.noreply.github.com>
2024-02-28 20:20:58 -0600

    Create global.js

---
+++ global.js
@@ -0,0 +1,9 @@
+document.querySelector("close")addEventListener("click",function()
+{
+console.log("click")
+document.querySelector("ventana-open").classList.remove("open")
+//document.queryselector(".ventana-open").classList.add("open")
+
+
+
+})
