640f99fc356e0359a9b9aa2ac86299fef9fa4a48
Hpzv26 <159396342+Hpzv26@users.noreply.github.com>
2024-03-20 19:59:10 -0600

    Update jsBasicos.js

--- jsBasicos.js
+++ jsBasicos.js
@@ -30,5 +30,36 @@ console.log(i);
 }
 
 //arrays
-let frutas=["manzana","banana","cereza"];
+let frutas=["manzana","banana","cereza","guayaba","aguacate","uva","mango","durazno","pera","sandia"];
 console.log(frutas[0]); //imprime "manzana"
+//objetos
+
+let persona={nombre:"juan",edad:30,saludo:function(){return "hola, mi nombre es " + this.nombre}};
+console.log(persona.saludo());
+
+//objetos
+document.querySelector("button").addEventListener("click",function(){alert("¡Boton presionado!");});
+
+/*
+eventos del mouse
+click: se dispara cuando el usuario hace clic en un elemento.
+dblclick: se dispara cuando el usuario hace doble clic en un elemento.
+mousedown:se dispara cuando el usuario presiona un boton del mouse sobre un elemento.
+mouseup: se dispara cuando el usuario suelta un boton del mouse sobre un elemento.
+mouseover: se dispara cuando el cursor del mouse se mueve sobre un elemento.
+mouseout: se dispara cuando el cursor del mouse se mueve fuera de un elemento.
+mousemove: se dispara cuando el cursor del mouse se mueve sobre un elemento.
+mousewheel:se dispara cuando el usuario gira la rueda del mouse 4.
+
+eventos de teclado
+keydown:se dispara cuando el usuario presiona una tecla.
+keyup: se dispara cuando el usuario suelta una tecla.
+keypress:se dispara cuando el usuario presiona y suelta una tecla 4.
+*/
+
+//HTML
+//<input placeholder="Haz clic aqui y luego presiona una tecla." size="40"/>
+//<p id="long"></p>
+//JS
+const input=document.querySelector("input");
+const log=document.getElementById("log");
