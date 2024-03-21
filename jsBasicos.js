b759b222a6435cb7a1d6c160e7f2a2770f6e876c
Hpzv26 <159396342+Hpzv26@users.noreply.github.com>
2024-03-20 19:34:52 -0600

    Create jsBasicos.js

---
+++ jsBasicos.js
@@ -0,0 +1,30 @@
+//consola
+console.log("hello world");
+//declaracion de variables
+var nombre="Juan";
+let edad =25;
+const PI=3.14159;
+
+//funciones
+function saludo(nombre){
+return "hola,"+nombre;
+
+
+}
+console.log(saludo("Mariana"));
+
+
+
+//condicionales
+let numero=10;
+if(numero>5)
+{
+console.log("el numero es mayo que 5");
+} else{
+    console.log("el numero es menor o igual a 5";)
+}
+
+//bucles 
+for(let i=0;i<5;i++){
+console.log(i);
+}
